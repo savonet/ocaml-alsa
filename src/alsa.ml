9995d1a14aaa02d3328f638460ffafc7ab9b4a5e
Samuel Mimram <smimram@gmail.com>
2019-08-28 14:02:48 +0200

    Correct headers.

--- src/alsa.ml
+++ src/alsa.ml
@@ -3,22 +3,20 @@
 
    This file is part of Ocaml-alsa.
 
-   Ocaml-shout is free software; you can redistribute it and/or modify
+   Ocaml-alsa is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
 
-   Ocaml-shout is distributed in the hope that it will be useful,
+   Ocaml-alsa is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with Ocaml-shout; if not, write to the Free Software
+   along with Ocaml-alsa; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 *)
-
-(* $Id$ *)
 
 external get_version : unit -> string = "ocaml_alsa_version"
 
