851ece953e2e8c18a5f681db7d0413b4f44c9f62
Samuel Mimram <smimram@gmail.com>
2020-07-29 09:39:59 +0200

    Sine example.

---
+++ examples/sine/sine.ml
@@ -0,0 +1,29 @@
+open Alsa
+
+let samplerate = 44100
+let channels = 2
+
+let () =
+  Printf.printf "Using ALSA %s.\n%!" (Alsa.get_version ());
+  let dev = Pcm.open_pcm "default" [Pcm.Playback] [] in
+  let params = Pcm.get_params dev in
+  Pcm.set_access dev params Pcm.Access_rw_interleaved;
+  Pcm.set_format dev params Pcm.Format_float;
+  let samplerate = Pcm.set_rate_near dev params samplerate Dir_eq in
+  Pcm.set_channels dev params channels;
+  Pcm.set_params dev params;
+  let period_size = Pcm.get_period_size params in
+  Printf.printf "samplerate: %d, period size: %d\n%!" samplerate period_size;
+  Pcm.prepare dev;
+  let buf = Bigarray.Array1.create Bigarray.Float32 Bigarray.C_layout (channels * period_size) in
+  let t = ref 0. in
+  while true do
+    for i = 0 to period_size - 1 do
+      let x = sin (2. *. Float.pi *. !t *. 440.) in
+      t := !t +. 1. /. float samplerate;
+      for c = 0 to channels - 1 do
+        buf.{channels*i+c} <- x
+      done
+    done;
+    ignore (Pcm.writei_float_ba dev channels buf)
+  done
